ce8eee9cf9f8de79144c701354f6516b650eaedf
lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2022-04-05 14:49:23 -0300

    adicionando a classe Diretor

--- src/classes/Diretor.java
+++ src/classes/Diretor.java
@@ -1,4 +1,32 @@
 package classes;
 
-public class Diretor {
+public class Diretor extends Pessoa{
+
+    protected String registroEducacao;
+    protected  int tempoDirecao;
+    protected String titulacao;
+
+    public String getRegistroEducacao() {
+        return registroEducacao;
+    }
+
+    public void setRegistroEducacao(String registroEducacao) {
+        this.registroEducacao = registroEducacao;
+    }
+
+    public int getTempoDirecao() {
+        return tempoDirecao;
+    }
+
+    public void setTempoDirecao(int tempoDirecao) {
+        this.tempoDirecao = tempoDirecao;
+    }
+
+    public String getTitulacao() {
+        return titulacao;
+    }
+
+    public void setTitulacao(String titulacao) {
+        this.titulacao = titulacao;
+    }
 }
